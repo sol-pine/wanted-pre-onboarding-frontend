596f71ddf13df3aa4c4db43cde87df87ca8c39e6
sol <osunpine@gmail.com>
2023-02-10 16:34:21 +0900

    feat. 프로젝트 패키지 설치, 페이지 경로 설정, 회원가입 유효성 검사 (#1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import ListPage from "./pages/ListPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path='/signup' element={<SignUpPage/>}/>
+      <Route path='/signin' element={<SignInPage/>}/>
+      <Route path='/todo' element={<ListPage/>}/>
+    </Routes>
     </div>
   );
 }
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+const SignUpPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    // 이메일 유효성 검사
+    const isEmailValid = () => {
+        const regExp = /@/;
+        return regExp.test(email);
+    };
+
+    // 비밀번호 유효성 검사
+    const isPasswordValid = () => {
+        const regExp = /^.{8,}$/;
+        return regExp.test(password);
+    };
+
+    return (
+        <>
+            <h1>회원가입</h1>
+            <input data-testid="email-input" type="text" onChange={(e) => setEmail(e.target.value)}/>
+            {email && !isEmailValid() ? <p>이메일 형식을 확인해주세요(@ 필수 포함)</p> : null}
+            <br/>
+            <input data-testid="password-input" type="password" onChange={(e) => setPassword(e.target.value)}/>
+            {password && !isPasswordValid() ? <p>비밀번호 형식을 확인해주세요(8자 이상)</p> : null}
+            <br/>
+            <button data-testid="signup-button">
+                회원가입
+            </button>
+        </>
+    );
+};
+
+export default SignUpPage;
