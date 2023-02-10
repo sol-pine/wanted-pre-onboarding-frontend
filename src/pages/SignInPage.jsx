18f273612a7e2520784c78637d3836573707b1ea
sol <osunpine@gmail.com>
2023-02-10 16:54:53 +0900

    feat. 로그인 유효성 검사, 유효성 검사에 따른 로그인 버튼 비활성화 처리 (#3)

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignInPage = () => {
-    return (
-        <div>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-        </div>
+    // 이메일 유효성 검사
+    const isEmailValid = () => {
+        const regExp = /@/;
+        return regExp.test(email);
+    };
+
+    // 비밀번호 유효성 검사
+    const isPasswordValid = () => {
+        const regExp = /^.{8,}$/;
+        return regExp.test(password);
+    };
+
+    return (
+        <>
+            <h1>로그인</h1>
+            <input data-testid="email-input" type="text" onChange={(e) => setEmail(e.target.value)}/>
+            {email && !isEmailValid() ? <p>이메일 형식을 확인해주세요(@ 필수 포함)</p> : null}
+            <br/>
+            <input data-testid="password-input" type="password" onChange={(e) => setPassword(e.target.value)}/>
+            {password && !isPasswordValid() ? <p>비밀번호 형식을 확인해주세요(8자 이상)</p> : null}
+            <br/>
+            <button data-testid="signin-button" disabled={!isEmailValid() || !isPasswordValid()}>
+                로그인
+            </button>
+        </>
     );
 };
 
