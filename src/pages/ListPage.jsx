71a7c35c5b0af0e99b31250677c8b26e022a843a
sol <osunpine@gmail.com>
2023-02-10 18:11:06 +0900

    feat. 투두 post, 투두 리스트 get (#6, #7)

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,20 +1,51 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
+import {createTodo, getTodos} from "../router/apis";
 
 const ListPage = () => {
-    const navigate = useNavigate();
-    const token = localStorage.getItem("token");
+        const navigate = useNavigate();
+        const [todo, setTodo] = useState("");
+        const [todos, setTodos] = useState([]);
+        const token = localStorage.getItem("token");
 
-    // 로그인 여부에 따른 리다이렉트 처리
-    useEffect(() => {
-        if (!token) navigate("/signin");
-    }, [token])
+        // 로그인 여부에 따른 리다이렉트 처리
+        useEffect(() => {
+            if (!token) navigate("/signin");
+        }, [token])
 
-    return (
-        <div>
+        // 투두 목록 받아오기
+        useEffect(() => {
+            getTodos().then((response) => setTodos(response.data));
+        }, [])
 
-        </div>
-    );
-};
+        // 투두 추가
+        const handleCreate = () => {
+            createTodo(todo).then(() => getTodos().then((response) => setTodos(response.data)));
+        }
+
+        return (
+            <>
+                <h1>TODO LIST</h1>
+                <input data-testid="new-todo-input" type="text" onChange={e => setTodo(e.target.value)}/>
+                <button data-testid="new-todo-add-button" onClick={handleCreate}>추가</button>
+                <br/>
+                <br/>
+                <br/>
+                {todos.map(({id, todo, isCompleted}) => (
+                    <>
+                        <li id={id}>
+                            <label>
+                                <input type="checkbox" checked={isCompleted}/>
+                                <span>{todo}</span>
+                            </label>
+                            <button data-testid="modify-button">수정</button>
+                            <button data-testid="delete-button">삭제</button>
+                        </li>
+                    </>
+                ))}
+            </>
+        );
+    }
+;
 
 export default ListPage;
--- src/router/apis.js
+++ src/router/apis.js
@@ -3,3 +3,7 @@ import axiosInstance from "./instance";
 export const signUp = (email, password) => axiosInstance.post("/auth/signup", {email: email, password: password});
 
 export const signIn = (email, password) => axiosInstance.post("/auth/signin", {email: email, password: password});
+
+export const createTodo = (todo) => axiosInstance.post("/todos", {todo: todo});
+
+export const getTodos = () => axiosInstance.get("/todos");
