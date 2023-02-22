dea9c0094fccdc87bd35aafa32fe60b0da1a9cb1
조해솔 <105091138+sol-pine@users.noreply.github.com>
2023-02-23 01:55:43 +0900

    fix. 유효성 검사 에러 메시지 출력 수정

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
-import {signUp} from "../utils/apis";
-import {useNavigate} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { signUp } from "../utils/apis";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import Input from "../components/Input";
 import Button from "../components/Button";
@@ -8,37 +8,34 @@ import isEmailValid from "../utils/isEmailValid";
 import isPasswordValid from "../utils/isPasswordValid";
 
 const SignUpPage = () => {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const token = localStorage.getItem("token");
 
-    // 로그인 여부에 따른 리다이렉트 처리
-    useEffect(() => {
-        if (token) navigate("/todo");
-    }, [token])
+  // 로그인 여부에 따른 리다이렉트 처리
+  useEffect(() => {
+    if (token) navigate("/todo");
+  }, [token]);
 
-    // 회원가입
-    const handleSignUp = () => {
-        signUp(email, password).then(() => navigate("/signin"))
-    }
+  // 회원가입
+  const handleSignUp = () => {
+    signUp(email, password).then(() => navigate("/signin"));
+  };
 
-    return (
-        <Container>
-            <Title>회원가입 페이지</Title>
-            <Input data-testid="email-input" type="text" onChange={(e) => setEmail(e.target.value)}
-                   placeholder="이메일을 입력해주세요 (@ 필수 포함)"/>
-            {email && !isEmailValid(email) ? <ErrorMsg>이메일 형식을 확인해주세요</ErrorMsg> : null}
-            <Input data-testid="password-input" type="password" onChange={(e) => setPassword(e.target.value)}
-                   placeholder="비밀번호를 입력해주세요 (8자 이상)" isNotValid={password && !isPasswordValid(password)}
-                   message="비밀번호 형식을 확인해주세요"/>
-            <br/>
-            <Button data-testid="signup-button"  onClick={handleSignUp}
-                    disabled={!isEmailValid(email) || !isPasswordValid(password)}>
-                회원가입
-            </Button>
-        </Container>
-    );
+  return (
+    <Container>
+      <Title>회원가입 페이지</Title>
+      <Input data-testid="email-input" type="text" onChange={(e) => setEmail(e.target.value)} placeholder="이메일을 입력해주세요 (@ 필수 포함)" />
+      {email && !isEmailValid(email) ? <ErrorMsg>이메일 형식을 확인해주세요</ErrorMsg> : null}
+      <Input data-testid="password-input" type="password" onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호를 입력해주세요 (8자 이상)" />
+      {password && !isPasswordValid(password) ? <ErrorMsg>비밀번호 형식을 확인해주세요</ErrorMsg> : null}
+      <br />
+      <Button data-testid="signup-button" onClick={handleSignUp} disabled={!isEmailValid(email) || !isPasswordValid(password)}>
+        회원가입
+      </Button>
+    </Container>
+  );
 };
 
 export default SignUpPage;
@@ -51,15 +48,15 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   gap: 10px;
-`
+`;
 
 const Title = styled.p`
   font-size: 20px;
   font-weight: 600;
   margin: 40px;
-`
+`;
 
 const ErrorMsg = styled.p`
   font-size: 12px;
-  color: #4461F2;
-`
+  color: #4461f2;
+`;
