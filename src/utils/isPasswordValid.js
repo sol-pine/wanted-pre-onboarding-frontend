8187ba72e307c954e939ebb6d5c2ed9f7ae16f6d
조해솔 <105091138+sol-pine@users.noreply.github.com>
2023-02-23 01:54:30 +0900

    fix. 유효성 검사 에러 메시지 출력 수정

--- src/utils/isPasswordValid.js
+++ src/utils/isPasswordValid.js
@@ -1,7 +1,7 @@
 // 비밀번호 유효성 검사
 const isPasswordValid = (password) => {
-    const regExp = /^.{8,}$/;
-    return regExp.test(password);
+  const regExp = new RegExp("(?=.{8,})");
+  return regExp.test(password);
 };
 
 export default isPasswordValid;
