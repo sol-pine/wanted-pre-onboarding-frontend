e53cdb071a32aa3143b7e3b4f131bfc13ed3262a
조해솔 <105091138+sol-pine@users.noreply.github.com>
2023-02-23 01:54:04 +0900

    fix. 유효성 검사 에러 메시지 출력 수정

--- src/utils/isEmailValid.js
+++ src/utils/isEmailValid.js
@@ -1,7 +1,7 @@
 // 이메일 유효성 검사
 const isEmailValid = (email) => {
-    const regExp = /@/;
-    return regExp.test(email);
+  const regExp = new RegExp("@");
+  return regExp.test(email);
 };
 
 export default isEmailValid;
